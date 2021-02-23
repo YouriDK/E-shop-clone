284f2d75562b61df2e3e20d70ca8a570f07a0f01
YouriDK <youri.choucoutou@gmail.com>
2021-02-23 14:54:29 +0100

    Heroku try 5

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ import orderRoute from "./routes/orderRoutes.js";
 import path from "path";
 
 doten.config();
-
-const mongodb_Url = config.MONGODB_URI || config.MONGODB_URL;
+// *  Mongo DB URI c'est le mongo DB atlas pas le compass en local
+const MONGODB_URI = config.MONGODB_URI;
 
 mongoose
-  .connect(mongodb_Url, {
+  .connect(MONGODB_URI, {
     useNewUrlParser: true,
     // * Pour enlever les warnings
     useUnifiedTopology: true,
@@ -28,33 +28,30 @@ mongoose.connection.on("connected", () => {
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// * Middleware pour changer entre developpement et production
-if (process.env.NODE_ENV === "production ") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Api Running ! ");
-  });
-}
-
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/product", productRoute); // ! Il fallait mettre cela pour pouvoir voir les détais du produit
 
+// * Middleware pour changer entre developpement et production
+if (process.env.NODE_ENV === "production ") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname + "../", "frontend", "build", "index.html")
+    );
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api Running !!! ");
+  });
+}
+
 app.get("/api/config/paypal", (req, res) => {
   //* Pour récupérer le client ID paypal dans le back end
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
-
-// ! express understand only ES5 and we're in ES6  that's why we need babel
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-}
 
 app.listen(PORT, () => {
   console.log(`Serve at http://localhost:${PORT}`);
