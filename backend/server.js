2253ae7d2ed332c685b4dd8b89d8189587017097
YouriDK <youri.choucoutou@gmail.com>
2021-02-23 14:03:43 +0100

    heroku try 3

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import userRoute from "./routes/userRoutes.js";
 import productRoute from "./routes/productsRoutes.js";
 import orderRoute from "./routes/orderRoutes.js";
+import path from "path";
 
 doten.config();
 
@@ -21,33 +22,36 @@ mongoose
   .catch((error) => console.log(error));
 
 mongoose.connection.on("connected", () => {
-  console.log("Mongoose is connected ! ");
+  console.log("Mongoose is connected ! ", process.env.NODE_ENV);
 });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// * Middleware pour changer entre developpement et production
+if (process.env.NODE_ENV === "production ") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api Running ! ");
+  });
+}
+
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/product", productRoute); // ! Il fallait mettre cela pour pouvoir voir les détais du produit
-/*
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);*/
-// app.get('/', (req, res) => {
-//   res.send('Server is ready');
-// });
 
 app.get("/api/config/paypal", (req, res) => {
   //* Pour récupérer le client ID paypal dans le back end
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
-/* 
-! express understand only ES5 and we're in ES6  that's why we need babel*/
-
+// ! express understand only ES5 and we're in ES6  that's why we need babel
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
 }
